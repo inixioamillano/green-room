31db82ceec03367ea9ccb12de9e7a9700ec5eefc
Inixio <inixio97@gmail.com>
2021-05-14 17:04:52 +0200

    Configure heroku deploy

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/green-room-app'));
+app.use(express.static(__dirname + '/dist/green-room'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/green-room-app/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/green-room/index.html'));
 });
 
 // default Heroku port
