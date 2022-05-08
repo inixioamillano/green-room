c7eea9c62e6ff5306b04f0e65ec9a75d742002d3
Luis Mayo Valbuena <luismayovalbuena@outlook.es>
2022-05-08 14:58:27 +0200

    Redirect to landing URL after login

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,12 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { NavigationCancel, NavigationEnd, Router } from '@angular/router';
 import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  
+
+  savedUrl: string;
+
   getUserId() {
     return localStorage.getItem('green-room-user-id');
   }
@@ -15,7 +18,19 @@ export class UserService {
     return localStorage.setItem('green-room-user-id', id);
   }
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) {
+    router.events.subscribe((event) => {
+      if (event instanceof NavigationCancel) {
+        // Saving URL from canceled route.
+        this.savedUrl = event.url;
+      } else if (event instanceof NavigationEnd) {
+        // If user navigates away from sign-in page, clearing saved URL.
+        if (!event.url.match(/^\/newuser/)) {
+          this.savedUrl = null;
+        }
+      }
+    });
+  }
 
   isLoggedIn() {
     return localStorage.getItem('green-room-user-id') !== null;
